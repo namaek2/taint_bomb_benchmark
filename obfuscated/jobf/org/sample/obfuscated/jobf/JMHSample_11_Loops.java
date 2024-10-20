61406966f916c175d1102df7e12c4d80379e4743
김남혁 / 金南赫 / NamHyeok Kim <34965961+namaek2@users.noreply.github.com>
2024-10-21 04:10:39 +0900

    Update JMHSample_11_Loops.java

--- obfuscated/jobf/org/sample/obfuscated/jobf/JMHSample_11_Loops.java
+++ obfuscated/jobf/org/sample/obfuscated/jobf/JMHSample_11_Loops.java
@@ -1,4 +1,4 @@
-package org.sample.obfuscated.jobf;
+package org.sample;
 
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -10,94 +10,96 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode({Mode.AverageTime})
 @State(Scope.Thread)
-/* loaded from: aa.zip:org/sample/JMHSample_11_Loops.class */
+/* loaded from: test.jar:org/sample/JMHSample_11_Loops.class */
 public class JMHSample_11_Loops {
-    private static final/* synthetic */ int[] llIll = null;
-    /* synthetic */ int x = llIll[0];
-    /* synthetic */ int y = llIll[1];
+    private static /* synthetic */ int[] llll = null;
+    /* synthetic */ int x = llll[0];
+    /* synthetic */ int y = llll[1];
 
-    @Benchmark
-    @OperationsPerInvocation(10)
-    public int measureWrong_10() {
-        return reps(llIll[3]);
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(1000)
-    public int measureWrong_1000() {
-        return reps(llIll[5]);
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(10000)
-    public int measureWrong_10000() {
-        return reps(llIll[6]);
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(1)
-    public int measureWrong_1() {
-        return reps(llIll[0]);
-    }
-
-    static {
-        lIll();
-    }
-
-    private static boolean Illl(int i, int i2) {
-        return i < i2;
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(100)
-    public int measureWrong_100() {
-        return reps(llIll[4]);
-    }
-
-    private static void lIll() {
-        llIll = new int[8];
-        llIll[0] = " ".length();
-        llIll[1] = "  ".length();
-        llIll[2] = (185 ^ 154) & ((71 ^ 100) ^ (-1));
-        llIll[3] = (((112 + 106) - 145) + 92) ^ (((41 + 167) - 131) + 98);
-        llIll[4] = (205 ^ 164) ^ (163 ^ 174);
-        llIll[5] = (-((-19521) & 27720)) & (-20497) & 29695;
-        llIll[6] = (-4271) & 14270;
-        llIll[7] = (-((-20931) & 29134)) & (-18453) & 126655;
-    }
-
-    public static void main(String[] llIIIlIllllllll) throws RunnerException {
-        new Runner(new OptionsBuilder().include(JMHSample_11_Loops.class.getSimpleName()).forks(llIll[0]).build()).run();
-        "".length();
-    }
-
-    private int reps(int llllllIllllllll) {
-        int IlllllIllllllll = llIll[2];
-        int lIIIIIlllllllll = llIll[2];
-        while (Illl(lIIIIIlllllllll, llllllIllllllll)) {
-            IlllllIllllllll += this.x + this.y;
-            lIIIIIlllllllll++;
-            "".length();
-            if (0 != 0) {
-                return (72 ^ 2) & ((49 ^ 123) ^ (-1));
-            }
-        }
-        return IlllllIllllllll;
-    }
-
-    @Benchmark
-    @OperationsPerInvocation(100000)
-    public int measureWrong_100000() {
-        return reps(llIll[7]);
+    private static void lIlI() {
+        llll = new int[8];
+        llll[0] = " ".length();
+        llll[1] = "  ".length();
+        llll[2] = ((38 ^ 68) ^ (100 ^ 53)) & (((106 ^ 0) ^ (251 ^ 162)) ^ (-" ".length()));
+        llll[3] = (((70 + 122) - 95) + 30) ^ (19 ^ 102);
+        llll[4] = 45 ^ 73;
+        llll[5] = (-31750) & 32749;
+        llll[6] = (-6287) & 16286;
+        llll[7] = (-((-2162) & 22647)) & (-10331) & 130815;
     }
 
     @Benchmark
     public int measureRight() {
         return this.x + this.y;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(1000)
+    public int measureWrong_1000() {
+        return reps(llll[5]);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(100)
+    public int measureWrong_100() {
+        return reps(llll[4]);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(10000)
+    public int measureWrong_10000() {
+        return reps(llll[6]);
+    }
+
+    private int reps(int llllllllllIlIlI) {
+        int llllllllllIlIIl = llll[2];
+        int llllllllllIllII = llll[2];
+        while (lIll(llllllllllIllII, llllllllllIlIlI)) {
+            llllllllllIlIIl += this.x + this.y;
+            llllllllllIllII++;
+            "".length();
+            if (((179 ^ 160) ^ (123 ^ 108)) <= 0) {
+                return ((98 ^ 55) ^ (237 ^ 149)) & (((69 ^ 4) ^ (250 ^ 150)) ^ (-" ".length()));
+            }
+        }
+        return llllllllllIlIIl;
+    }
+
+    private static boolean lIll(int i, int i2) {
+        return i < i2;
+    }
+
+    public static void main(String[] lllllllllIlIIIl) throws RunnerException {
+        Options lllllllllIlIIII = new OptionsBuilder().include(JMHSample_11_Loops.class.getSimpleName()).forks(llll[0]).build();
+        new Runner(lllllllllIlIIII).run();
+        "".length();
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(1)
+    public int measureWrong_1() {
+        return reps(llll[0]);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(100000)
+    public int measureWrong_100000() {
+        return reps(llll[7]);
+    }
+
+    static {
+        lIlI();
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(10)
+    public int measureWrong_10() {
+        return reps(llll[3]);
     }
 }
